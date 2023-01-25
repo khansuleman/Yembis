cce290bbb5f117cfd4675df733c749ceb9f92ed4
yunus kurban <71645029+yunuskurban@users.noreply.github.com>
2023-01-25 09:26:33 +0100

    upload

--- AI/speedradar2/SpeedRadar2.py
+++ AI/speedradar2/SpeedRadar2.py
@@ -8,9 +8,7 @@ end = 0
 tracker = EuclideanDistTracker()
 
 #cap = cv2.VideoCapture(0)
-cap = cv2.VideoCapture("../speedradar/traffic4.mp4")
-
-#cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture('test.mp4')
 
 
 f = 25
@@ -19,7 +17,6 @@ w = int(1000/(f-1))
 
 #Object Detection
 object_detector = cv2.createBackgroundSubtractorMOG2(history=None,varThreshold=None)
-#100,5
 
 #KERNALS
 kernalOp = np.ones((3,3),np.uint8)
@@ -32,27 +29,16 @@ while True:
     ret,frame = cap.read()
     if not ret:
         break
-    
-    # camera laptop
-    # frame = cv2.resize(frame, None, fx=2, fy=2)
-
-    # video
-    frame = cv2.resize(frame, None, fx=0.5, fy=0.5)
 
     height,width,_ = frame.shape
-    #print(height,width)
-    #540,960
-
-
-    #Extract ROI
-    roi = frame[50:540,200:960]
+    print(height,width)
 
     #MASKING METHOD 1
-    mask = object_detector.apply(roi)
+    mask = object_detector.apply(frame)
     _, mask = cv2.threshold(mask, 250, 255, cv2.THRESH_BINARY)
 
     #DIFFERENT MASKING METHOD 2 -> This is used
-    fgmask = fgbg.apply(roi)
+    fgmask = fgbg.apply(frame)
     ret, imBin = cv2.threshold(fgmask, 200, 255, cv2.THRESH_BINARY)
     mask1 = cv2.morphologyEx(imBin, cv2.MORPH_OPEN, kernalOp)
     mask2 = cv2.morphologyEx(mask1, cv2.MORPH_CLOSE, kernalCl)
@@ -65,41 +51,34 @@ while True:
     for cnt in contours:
         area = cv2.contourArea(cnt)
         #THRESHOLD
-        if area > 1000:
+        if area > 800:
             x,y,w,h = cv2.boundingRect(cnt)
-            cv2.rectangle(roi,(x-10,y-10),(x+w+10,y+h+10),(0,255,0),3)
+            cv2.rectangle(frame,(x-10,y-10),(x+w+10,y+h+10),(0,255,0),3)
             detections.append([x,y,w,h])
 
     #Object Tracking
     boxes_ids = tracker.update(detections)
     for box_id in boxes_ids:
         x,y,w,h,id = box_id
-
-
-        if(tracker.getsp(id)<tracker.limit()):
-            cv2.putText(roi,str(id)+" "+str(tracker.getsp(id)),(x,y-15), cv2.FONT_HERSHEY_PLAIN,1,(255,255,0),2)
-            cv2.rectangle(roi,(x-10,y-10),(x+w+10,y+h+10),(0,255,0),3)
-        else:
-            cv2.putText(roi,str(id)+ " "+str(tracker.getsp(id)),(x, y-15),cv2.FONT_HERSHEY_PLAIN, 1,(0, 0, 255),2)
-            cv2.rectangle(roi,(x-10,y-10),(x+w+10,y+h+10),(0,165,255),3)
+        
+        cv2.putText(frame,str(id)+" "+str(tracker.getsp(id)),(x,y-15), cv2.FONT_HERSHEY_PLAIN,1,(255,255,0),2)
+        cv2.rectangle(frame,(x-10,y-10),(x+w+10,y+h+10),(0,255,0),3)
 
         s = tracker.getsp(id)
         if (tracker.f[id] == 1 and s != 0):
-            tracker.capture(roi, x, y, h, w, s, id)
+            tracker.capture(frame, x, y, h, w, s, id)
 
     # DRAW LINES
+    cv2.line(frame, (0, (int(line1) - 20)), (640, (int(line1) - 20)), (0, 0, 255), 2)
+    cv2.line(frame, (0, int(line1)), (640, int(line1)), (0, 0, 255), 2)
 
-    cv2.line(roi, (0, 410), (960, 410), (0, 0, 255), 2)
-    cv2.line(roi, (0, 430), (960, 430), (0, 0, 255), 2)
-
-    cv2.line(roi, (0, 235), (960, 235), (0, 0, 255), 2)
-    cv2.line(roi, (0, 255), (960, 255), (0, 0, 255), 2)
+    cv2.line(frame, (0, (int(line1) - int(distance1))), (640, (int(line1) - int(distance1))), (0, 0, 255), 2)
+    cv2.line(frame, (0, ((int(line1) - 20) - int(distance1))), (640, ((int(line1) - 20) - int(distance1))), (0, 0, 255), 2)
 
 
     #DISPLAY
-    #cv2.imshow("Mask",mask2)
-    #cv2.imshow("Erode", e_img)
-    cv2.imshow("ROI", roi)
+    cv2.imshow("Frame", frame)
+    cv2.imshow("fgbg", fgmask)
 
     key = cv2.waitKey(w-10)
     if key==27:
