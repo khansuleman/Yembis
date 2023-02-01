e2ae6167eb0e830422c21f00d507b881557fa507
EmileDV <r0851531@student.thomasmore.be>
2023-02-01 10:16:59 +0100

    begin forms

---
+++ APP/yembis_app/src/app/cameras/cameras.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cameras',
+  templateUrl: './cameras.component.html',
+  styleUrls: ['./cameras.component.css']
+})
+export class CamerasComponent {
+
+}
---
+++ APP/yembis_app/src/app/start/start.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-start',
+  templateUrl: './start.component.html',
+  styleUrls: ['./start.component.css']
+})
+export class StartComponent {
+}
