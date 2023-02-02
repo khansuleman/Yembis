f96a627ebb80571cc9346c19da2c37023987b595
EmileDV <r0851531@student.thomasmore.be>
2023-02-02 16:56:39 +0100

    alleen functionaliteit van from nog

--- APP/yembis_app/src/app/app.module.ts
+++ APP/yembis_app/src/app/app.module.ts
@@ -3,17 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CamerasComponent } from './cameras/cameras.component';
+
 import { StartComponent } from './start/start.component';
-import { MenuComponent } from './menu/menu.component';
-import { CameramodalComponent } from './cameramodal/cameramodal.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
     StartComponent,
-    CameramodalComponent,
   ],
   imports: [
     BrowserModule,
