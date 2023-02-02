90c461d0488f394bb6b50a273b2742579a3ef846
Geukens Michiel <michiel.geukens@gmail.com>
2023-02-02 10:58:45 +0100

    Update tracker2.py

--- AI/speedradar_model/tracker2.py
+++ AI/speedradar_model/tracker2.py
@@ -7,9 +7,15 @@ from datetime import datetime
 from PIL import Image, ImageOps #Install pillow instead of PIL
 from PIL import Image as im
 import numpy as np
+from edge_impulse_linux.image import ImageImpulseRunner
+import sys, getopt
+
+runner = None
+
+model = "/home/pi/.ei-linux-runner/models/181016/v1/model.eim"
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
-modelfile = os.path.join(dir_path, "model.eim")
+modelfile = os.path.join(dir_path, model)
 
 kastid = 0
 distance = 0
@@ -118,6 +124,7 @@ class EuclideanDistTracker:
 
         return int(s)
 
+
     # SAVE VEHICLE DATA
     def capture(self, img, x, y, h, w, sp, id):
         if (self.capf[id] == 0):
@@ -127,6 +134,58 @@ class EuclideanDistTracker:
             
             # save image to destination folder
             crop_img = img[y - 10:y + h + 10, x - 10:x + w + 10]
+
+            with ImageImpulseRunner(modelfile) as runner:
+                try:
+                    model_info = runner.init()
+                    print('Loaded runner for "' + model_info['project']['owner'] + ' / ' + model_info['project']['name'] + '"')
+                    labels = model_info['model_parameters']['labels']
+
+                    img = crop_img
+                    if img is None:
+                        print('Failed to load image')
+                        exit(1)
+
+                    # imread returns images in BGR format, so we need to convert to RGB
+                    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+                    # get_features_from_image also takes a crop direction arguments in case you don't have square images
+                    features, cropped = runner.get_features_from_image(img)
+
+                    res = runner.classify(features)
+
+                    if "classification" in res["result"].keys():
+                        print('Result (%d ms.) ' % (res['timing']['dsp'] + res['timing']['classification']), end='')
+                        predicted = ""
+                        predictedValue = 0
+                        for label in labels:
+                            
+                            score = res['result']['classification'][label]
+                            if score > predictedValue:
+                                predicted = label
+                                predictedValue = score
+                            
+
+                            print('%s: %.2f\t' % (label, score), end='')
+                        print('', flush=True)
+                        print(predicted, str(predictedValue))
+
+                    elif "bounding_boxes" in res["result"].keys():
+                        print('Found %d bounding boxes (%d ms.)' % (len(res["result"]["bounding_boxes"]), res['timing']['dsp'] + res['timing']['classification']))
+                        for bb in res["result"]["bounding_boxes"]:
+                            print('\t%s (%.2f): x=%d y=%d w=%d h=%d' % (bb['label'], bb['value'], bb['x'], bb['y'], bb['width'], bb['height']))
+                            cropped = cv2.rectangle(cropped, (bb['x'], bb['y']), (bb['x'] + bb['width'], bb['y'] + bb['height']), (255, 0, 0), 1)
+
+                    # the image will be resized and cropped, save a copy of the picture here
+                    # so you can see what's being passed into the classifier
+                    cv2.imwrite('debug.jpg', cv2.cvtColor(cropped, cv2.COLOR_RGB2BGR))
+
+                finally:
+                    if (runner):
+                        runner.stop()
+
+            
+            
             
             # # write object in csv file
             filet = open(speed_record_file_location, "a")
@@ -134,3 +193,4 @@ class EuclideanDistTracker:
             filet.write(str(kastid)+ ";"  + str(sp) + ";" + str(now.strftime("%d/%m/%Y %H:%M:%S")) + ";" + str("na") + "\n")
             filet.close()
 
+    # IMAGE CLASSIFICATION
